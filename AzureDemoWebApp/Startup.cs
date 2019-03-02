92b489e062abf5ddc29c96f46e9ae046a14c9b7e
sjagdale <sjagdale@LIN13002647>
2019-03-02 09:45:03 +0530

    Add project files.

---
+++ AzureDemoWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureDemoWebApp.Startup))]
+namespace AzureDemoWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
